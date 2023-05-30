2e8f38c5d77b99e73595e9b2837c9450e4ec7428
Luizsellitto <luizsellitto2609@gmail.com>
2023-05-30 14:59:01 -0400

    Teste Simples v1

---
+++ Teste Simples - Git/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Informe o primeiro valor!");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o segundo valor!");
+int b = int.Parse(Console.ReadLine());
+int result = a / b;
+Console.WriteLine($"O resultado da divisão dos dois é: {result}");
