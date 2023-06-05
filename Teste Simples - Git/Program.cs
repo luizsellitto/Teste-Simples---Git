732813a5a5b3882bc3a3098d0ff1b0dfc4fe72a6
Luizsellitto <luizsellitto2609@gmail.com>
2023-06-05 16:56:48 -0400

    Tipos de Catch

--- Teste Simples - Git/Program.cs
+++ Teste Simples - Git/Program.cs
@@ -1,13 +1,21 @@
-﻿Console.WriteLine("Informe o primeiro valor!");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Informe o segundo valor!");
-int b = int.Parse(Console.ReadLine());
-try
+﻿try
 {
-int result = a / b;
-Console.WriteLine($"O resultado da divisão dos dois é: {result}");
+    Console.WriteLine("Informe o primeiro valor!");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe o segundo valor!");
+    int b = int.Parse(Console.ReadLine());
+    int result = a / b;
+    Console.WriteLine($"O resultado da divisão dos dois é: {result}");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine(" Não se pode dividir um número por 0\n" + ex.Message);
+}
+catch (InvalidCastException ex)
+{
+    Console.WriteLine(" Não se porde digitar letras, por favor, digite um número\n" + ex.Message);
 }
 catch (Exception ex)
 {
-    Console.WriteLine(" Não se pode dividir um número por 0 " + ex.Message);
+    Console.WriteLine("Houve um erro\n" + ex.Message);
 }
